a15d99314ac533a35c63a6f56e5066aac5587cb7
Vinicius <ext.vinicius.nunes@farfetch.com>
2024-07-12 20:49:25 +0100

    feat: start project

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+import dynamic from "next/dynamic";
+import React from "react";
+import ChartOne from "../Charts/ChartOne";
+import ChartTwo from "../Charts/ChartTwo";
+import useColorMode from "@/hooks/useColorMode";
+
+const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
+    ssr: false,
+});
+
+const Dashboard: React.FC = () => {
+    const [colorMode] = useColorMode();
+
+    return (
+        <>
+            <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+                <ChartOne />
+                <ChartTwo />
+                <ChartThree />
+            </div>
+        </>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/Layouts/DefaultLayout.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState, ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
+
+export default function DefaultLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+
+                <div className="relative flex flex-1 flex-col lg:ml-72.5">
+
+                    <main>
+                        <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                            {children}
+                        </div>
+                    </main>
+                </div>
+            </div>
+        </>
+    );
+}
